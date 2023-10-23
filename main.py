d6199dd01eb187c5b8898d39b96e7c1f399742a3
Mike <mike.cabaluna@sait.edu.com>
2023-10-23 14:20:26 -0600

    dict

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 
-# txt file 
+# read the txt file 
 with open("ini.txt") as ini_file:
     data = ini_file.read()
 
+lines = data.split("\n")
+key_value_ini = dict()
+
+for line in lines:
+    if "=" in line:
+        fields = line.split("=")
+        key_value_ini[fields[0]]= fields[1]
